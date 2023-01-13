035cb3bc883ce62d7918f94fbdba0744951ea63a
Taiki Endo <te316e89@gmail.com>
2023-01-14 03:09:45 +0900

    Initial implementation

---
+++ README.md
@@ -0,0 +1,123 @@
+# cache-cargo-install-action
+
+[![release](https://img.shields.io/github/release/taiki-e/cache-cargo-install-action?style=flat-square&logo=github)](https://github.com/taiki-e/cache-cargo-install-action/releases/latest)
+[![build status](https://img.shields.io/github/actions/workflow/status/taiki-e/cache-cargo-install-action/ci.yml?branch=main&style=flat-square&logo=github)](https://github.com/taiki-e/cache-cargo-install-action/actions)
+
+GitHub Action for `cargo install` with cache.
+
+This is intended for installing crates that are not supported by [install-action].
+For performance and robustness, we recommend using [install-action] if the tool is supported by [install-action].
+
+- [Usage](#usage)
+  - [Inputs](#inputs)
+  - [Example workflow](#example-workflow)
+- [Migrate from/to install-action](#migrate-fromto-install-action)
+- [Compatibility](#compatibility)
+- [Related Projects](#related-projects)
+- [License](#license)
+
+## Usage
+
+### Inputs
+
+| Name | Required | Description         | Type    | Default |
+| ---- |:--------:| ------------------- | ------- | ------- |
+| tool | **true** | Crate to install    | String  |         |
+
+### Example workflow
+
+```yaml
+- uses: taiki-e/cache-cargo-install-action@v1
+  with:
+    tool: cargo-hack
+```
+
+To install a specific version, use `@version` syntax:
+
+```yaml
+- uses: taiki-e/cache-cargo-install-action@v1
+  with:
+    tool: cargo-hack@0.5.24
+```
+
+Omitting minor/patch versions is not supported yet.
+
+## Migrate from/to install-action
+
+This action provides an interface compatible with [install-action].
+
+Therefore, migrating from/to [install-action] is usually just a change of action to be used. (if the tool and version are supported by install-action or install-action's `binstall` fallback)
+
+To migrate from this action to install-action:
+
+```diff
+- - uses: taiki-e/cache-cargo-install-action@v1
++ - uses: taiki-e/install-action@v2
+    with:
+      tool: cargo-hack
+```
+
+To migrate from install-action to this action:
+
+```diff
+- - uses: taiki-e/install-action@v2
++ - uses: taiki-e/cache-cargo-install-action@v1
+    with:
+      tool: cargo-hack
+```
+
+The interface of this action is a subset of the interface of [install-action], so note the following limitations when migrating from install-action to this action.
+
+- install-action supports specifying multiple crates in a single action call, but this action does not.
+
+  For example, in install-action, you can write:
+
+  ```yaml
+  - uses: taiki-e/install-action@v2
+      with:
+        tool: cargo-hack,cargo-minimal-versions
+  ```
+
+  In this action, you need to write:
+
+  ```yaml
+  - uses: taiki-e/cache-cargo-install-action@v1
+      with:
+        tool: cargo-hack
+  - uses: taiki-e/cache-cargo-install-action@v1
+      with:
+        tool: cargo-minimal-versions
+  ```
+
+- install-action supports omitting minor/patch versions, but this action does not.
+
+- install-action supports `@<tool_name>` shorthand, but this action does not.
+
+## Compatibility
+
+This action has been tested for GitHub-hosted runners (Ubuntu, macOS, Windows) and containers (Ubuntu, Debian, Alpine, Fedora, CentOS, Rocky).
+To use this action in self-hosted runners or in containers, you will need to install at least the following:
+
+- bash
+- cargo
+
+## Related Projects
+
+- [install-action]: GitHub Action for installing development tools (mainly from GitHub Releases).
+- [create-gh-release-action]: GitHub Action for creating GitHub Releases based on changelog.
+- [upload-rust-binary-action]: GitHub Action for building and uploading Rust binary to GitHub Releases.
+- [setup-cross-toolchain-action]: GitHub Action for setup toolchains for cross compilation and cross testing for Rust.
+
+[create-gh-release-action]: https://github.com/taiki-e/create-gh-release-action
+[setup-cross-toolchain-action]: https://github.com/taiki-e/setup-cross-toolchain-action
+[upload-rust-binary-action]: https://github.com/taiki-e/upload-rust-binary-action
+[install-action]: https://github.com/taiki-e/install-action
+
+## License
+
+Licensed under either of [Apache License, Version 2.0](LICENSE-APACHE) or
+[MIT license](LICENSE-MIT) at your option.
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall
+be dual licensed as above, without any additional terms or conditions.
---
+++ pre.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+retry() {
+    for i in {1..5}; do
+        if "$@"; then
+            return 0
+        else
+            sleep "${i}"
+        fi
+    done
+    "$@"
+}
+bail() {
+    echo "::error::$*"
+    exit 1
+}
+apt_update() {
+    if type -P sudo &>/dev/null; then
+        retry sudo apt-get -o Acquire::Retries=10 -qq update
+    else
+        retry apt-get -o Acquire::Retries=10 -qq update
+    fi
+    apt_updated=1
+}
+apt_install() {
+    if [[ -z "${apt_updated:-}" ]]; then
+        apt_update
+    fi
+    if type -P sudo &>/dev/null; then
+        retry sudo apt-get -o Acquire::Retries=10 -qq -o Dpkg::Use-Pty=0 install -y --no-install-recommends "$@"
+    else
+        retry apt-get -o Acquire::Retries=10 -qq -o Dpkg::Use-Pty=0 install -y --no-install-recommends "$@"
+    fi
+}
+apk_install() {
+    if type -P doas &>/dev/null; then
+        doas apk add --no-cache "$@"
+    else
+        apk add --no-cache "$@"
+    fi
+}
+dnf_install() {
+    if type -P sudo &>/dev/null; then
+        retry sudo "${dnf}" install -y "$@"
+    else
+        retry "${dnf}" install -y "$@"
+    fi
+}
+sys_install() {
+    case "${base_distro}" in
+        debian) apt_install "$@" ;;
+        alpine) apk_install "$@" ;;
+        fedora) dnf_install "$@" ;;
+    esac
+}
+
+# Inputs
+tool="${INPUT_TOOL:?}"
+
+# Refs: https://github.com/rust-lang/rustup/blob/HEAD/rustup-init.sh
+base_distro=""
+case "$(uname -s)" in
+    Linux)
+        ldd_version=$(ldd --version 2>&1 || true)
+        if grep <<<"${ldd_version}" -q 'musl'; then
+            host_os="linux-musl"
+        else
+            host_glibc_version=$(grep <<<"${ldd_version}" -E "GLIBC|GNU libc" | sed "s/.* //g")
+            host_os="linux-gnu-${host_glibc_version}"
+        fi
+        if grep -q '^ID_LIKE=' /etc/os-release; then
+            base_distro="$(grep '^ID_LIKE=' /etc/os-release | sed 's/^ID_LIKE=//')"
+            case "${base_distro}" in
+                *debian*) base_distro=debian ;;
+                *alpine*) base_distro=alpine ;;
+                *fedora*) base_distro=fedora ;;
+            esac
+        else
+            base_distro="$(grep '^ID=' /etc/os-release | sed 's/^ID=//')"
+        fi
+        case "${base_distro}" in
+            fedora)
+                dnf=dnf
+                if ! type -P dnf &>/dev/null; then
+                    if type -P microdnf &>/dev/null; then
+                        # fedora-based distributions have "minimal" images that
+                        # use microdnf instead of dnf.
+                        dnf=microdnf
+                    else
+                        # If neither dnf nor microdnf is available, it is
+                        # probably an RHEL7-based distribution that does not
+                        # have dnf installed by default.
+                        dnf=yum
+                    fi
+                fi
+                ;;
+        esac
+        ;;
+    Darwin) host_os=macos ;;
+    MINGW* | MSYS* | CYGWIN* | Windows_NT) host_os=windows ;;
+    *) bail "unrecognized OS type '$(uname -s)'" ;;
+esac
+case "$(uname -m)" in
+    aarch64 | arm64) host_arch="aarch64" ;;
+    xscale | arm | armv6l | armv7l | armv8l)
+        # Ignore arm for now, as we need to consider the version and whether hard-float is supported.
+        # https://github.com/rust-lang/rustup/pull/593
+        # https://github.com/cross-rs/cross/pull/1018
+        # Does it seem only armv7l is supported?
+        # https://github.com/actions/runner/blob/caec043085990710070108f375cd0aeab45e1017/src/Misc/externals.sh#L174
+        bail "32-bit ARM runner is not supported yet by this action"
+        ;;
+    # GitHub Actions Runner supports Linux (x86_64, aarch64, arm), Windows (x86_64, aarch64),
+    # and macOS (x86_64, aarch64).
+    # https://github.com/actions/runner
+    # https://github.com/actions/runner/blob/caec043085990710070108f375cd0aeab45e1017/.github/workflows/build.yml#L21
+    # https://docs.github.com/en/actions/hosting-your-own-runners/about-self-hosted-runners#supported-architectures-and-operating-systems-for-self-hosted-runners
+    # So we can assume x86_64 unless it is aarch64 or arm.
+    *) host_arch="x86_64" ;;
+esac
+
+if [[ "${tool}" == *","* ]]; then
+    bail "cache-cargo-install-action does not support specifying multiple crates yet; consider calling this action per crate"
+fi
+if [[ "${tool}" == *"@"* ]]; then
+    version="${tool#*@}"
+    tool="${tool%@*}"
+    if [[ ! "${version}" =~ ^([1-9][0-9]*\.[0-9]+\.[0-9]+|0\.[1-9][0-9]*\.[0-9]+|^0\.0\.[0-9]+)(-[0-9A-Za-z\.-]+)?(\+[0-9A-Za-z\.-]+)?$|^latest$ ]]; then
+        if [[ ! "${version}" =~ ^([1-9][0-9]*(\.[0-9]+(\.[0-9]+)?)?|0\.[1-9][0-9]*(\.[0-9]+)?|^0\.0\.[0-9]+)(-[0-9A-Za-z\.-]+)?(\+[0-9A-Za-z\.-]+)?$|^latest$ ]]; then
+            bail "cache-cargo-install-action does not support non-semver version: '${version}'"
+        fi
+        bail "cache-cargo-install-action does not support omitting patch/minor version yet: '${version}'"
+    fi
+else
+    version="latest"
+fi
+
+case "${version}" in
+    latest)
+        if ! type -P jq &>/dev/null || ! type -P curl &>/dev/null; then
+            case "${base_distro}" in
+                debian | alpine) sys_install ca-certificates curl jq ;;
+                fedora)
+                    if [[ "${dnf}" == "yum" ]]; then
+                        # On RHEL7-based distribution jq requires EPEL
+                        sys_install ca-certificates curl epel-release
+                        sys_install jq --enablerepo=epel
+                    else
+                        sys_install ca-certificates curl jq
+                    fi
+                    ;;
+            esac
+        fi
+        version=$(retry curl --proto '=https' --tlsv1.2 -fsSL --retry 10 "https://crates.io/api/v1/crates/${tool}" | jq -r '.crate.max_stable_version')
+        ;;
+esac
+case "${base_distro}" in
+    # https://github.com/actions/cache/issues/479
+    alpine) sys_install tar ;;
+esac
+
+bin_dir="${RUNNER_TOOL_CACHE}/${tool}/bin"
+echo "${bin_dir}" >>"${GITHUB_PATH}"
+
+cat >>"${GITHUB_OUTPUT}" <<EOF
+tool=${tool}
+version=${version}
+key=${tool}-${version}-${host_arch}-${host_os}
+path=${bin_dir}
+EOF

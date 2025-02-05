taiki-e/cache-cargo-install-action

commit e7484e2d4deec5779a5c8f55f88cf522978898bd

Clean up scripts

pre.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: Apache-2.0 OR MIT
-set -eEuo pipefail
+set -CeEuo pipefail
 IFS=$'\n\t'
 
 retry() {
@@ -14,17 +14,17 @@ retry() {
     "$@"
 }
 bail() {
-    echo "::error::$*"
+    printf '::error::%s\n' "$*"
     exit 1
 }
 warn() {
-    echo "::warning::$*"
+    printf '::warning::%s\n' "$*"
 }
 info() {
-    echo "info: $*"
+    printf >&2 'info: %s\n' "$*"
 }
 _sudo() {
-    if type -P sudo &>/dev/null; then
+    if type -P sudo >/dev/null; then
         sudo "$@"
     else
         "$@"
@@ -34,14 +34,14 @@ download_and_checksum() {
     local url="${1:?}"
     local checksum="${2:?}"
     retry curl --proto '=https' --tlsv1.2 -fsSL --retry 10 "${url}" -o tmp
-    if type -P sha256sum &>/dev/null; then
+    if type -P sha256sum >/dev/null; then
         sha256sum -c - >/dev/null <<<"${checksum} *tmp"
-    elif type -P shasum &>/dev/null; then
+    elif type -P shasum >/dev/null; then
         # GitHub-hosted macOS runner does not install GNU Coreutils by default.
         # https://github.com/actions/runner-images/issues/90
         shasum -a 256 -c - >/dev/null <<<"${checksum} *tmp"
     else
-        warn "checksum requires 'sha256sum' or 'shasum' command; consider installing one of them; skipped checksum for $(basename "${url}")"
+        warn "checksum requires 'sha256sum' or 'shasum' command; consider installing one of them; skipped checksum for $(basename -- "${url}")"
     fi
 }
 apt_update() {
@@ -61,9 +61,9 @@ pacman_install() {
     retry _sudo pacman -Sy --noconfirm "$@"
 }
 apk_install() {
-    if type -P sudo &>/dev/null; then
+    if type -P sudo >/dev/null; then
         retry sudo apk --no-cache add "$@"
-    elif type -P doas &>/dev/null; then
+    elif type -P doas >/dev/null; then
         retry doas apk --no-cache add "$@"
     else
         retry apk --no-cache add "$@"
@@ -86,18 +86,18 @@ tag="${INPUT_TAG:-}"
 rev="${INPUT_REV:-}"
 
 # Refs: https://github.com/rust-lang/rustup/blob/HEAD/rustup-init.sh
-base_distro=""
+base_distro=''
 case "$(uname -s)" in
     Linux)
         ldd_version=$(ldd --version 2>&1 || true)
-        if grep <<<"${ldd_version}" -q 'musl'; then
+        if grep -Fq musl <<<"${ldd_version}"; then
             host_os="linux-musl"
         else
-            host_glibc_version=$(grep <<<"${ldd_version}" -E "GLIBC|GNU libc" | sed "s/.* //g")
+            host_glibc_version=$(grep -E "GLIBC|GNU libc" <<<"${ldd_version}" | sed -E "s/.* //g")
             host_os="linux-gnu-${host_glibc_version}"
         fi
-        if grep -q '^ID_LIKE=' /etc/os-release; then
-            base_distro=$(grep '^ID_LIKE=' /etc/os-release | cut -d= -f2)
+        if grep -Eq '^ID_LIKE=' /etc/os-release; then
+            base_distro=$(grep -E '^ID_LIKE=' /etc/os-release | cut -d= -f2)
             case "${base_distro}" in
                 *debian*) base_distro=debian ;;
                 *fedora*) base_distro=fedora ;;
@@ -105,13 +105,14 @@ case "$(uname -s)" in
                 *alpine*) base_distro=alpine ;;
             esac
         else
-            base_distro=$(grep '^ID=' /etc/os-release | cut -d= -f2)
+            base_distro=$(grep -E '^ID=' /etc/os-release | cut -d= -f2)
         fi
+        base_distro="${base_distro//\"/}"
         case "${base_distro}" in
             fedora)
                 dnf=dnf
-                if ! type -P dnf &>/dev/null; then
-                    if type -P microdnf &>/dev/null; then
+                if ! type -P dnf >/dev/null; then
+                    if type -P microdnf >/dev/null; then
                         # fedora-based distributions have "minimal" images that
                         # use microdnf instead of dnf.
                         dnf=microdnf
@@ -130,22 +131,22 @@ case "$(uname -s)" in
     *) bail "unrecognized OS type '$(uname -s)'" ;;
 esac
 case "$(uname -m)" in
-    aarch64 | arm64) host_arch="aarch64" ;;
+    aarch64 | arm64) host_arch=aarch64 ;;
     xscale | arm | armv*l)
-        # Ignore arm for now, as we need to consider the version and whether hard-float is supported.
+        # Ignore Arm for now, as we need to consider the version and whether hard-float is supported.
         # https://github.com/rust-lang/rustup/pull/593
         # https://github.com/cross-rs/cross/pull/1018
         # Does it seem only armv7l+ is supported?
-        # https://github.com/actions/runner/blob/v2.315.0/src/Misc/externals.sh#L189
+        # https://github.com/actions/runner/blob/v2.319.0/src/Misc/externals.sh#L191
         # https://github.com/actions/runner/issues/688
-        bail "32-bit ARM runner is not supported yet by this action; if you need support for this platform, please submit an issue at <https://github.com/taiki-e/cache-cargo-install-action>"
+        bail "32-bit Arm runner is not supported yet by this action; if you need support for this platform, please submit an issue at <https://github.com/taiki-e/cache-cargo-install-action>"
         ;;
-    # GitHub Actions Runner supports Linux (x86_64, aarch64, arm), Windows (x86_64, aarch64),
-    # and macOS (x86_64, aarch64).
-    # https://github.com/actions/runner/blob/v2.315.0/.github/workflows/build.yml#L21
+    # GitHub Actions Runner supports Linux (x86_64, AArch64, Arm), Windows (x86_64, AArch64),
+    # and macOS (x86_64, AArch64).
+    # https://github.com/actions/runner/blob/v2.319.0/.github/workflows/build.yml#L21
     # https://docs.github.com/en/actions/hosting-your-own-runners/about-self-hosted-runners#supported-architectures-and-operating-systems-for-self-hosted-runners
-    # So we can assume x86_64 unless it is aarch64 or arm.
-    *) host_arch="x86_64" ;;
+    # So we can assume x86_64 unless it is AArch64 or Arm.
+    *) host_arch=x86_64 ;;
 esac
 
 if [[ "${tool}" == *","* ]]; then
@@ -171,39 +172,38 @@ elif [[ -n "${git}" ]]; then
     if [[ -z "${tag}" ]] && [[ -z "${rev}" ]]; then
         bail "'git' input option currently requires one of 'tag' or 'rev' input option"
     fi
-    version=""
+    version=''
 else
-    version="latest"
+    version=latest
 fi
 
 case "${locked}" in
     true)
-        locked="--locked"
+        locked=--locked
         locked_key=''
         ;;
     false)
         locked=''
-        locked_key="-locked-false"
+        locked_key=-locked-false
         ;;
     *) bail "'locked' input option must be 'true' or 'false': '${locked}'" ;;
 esac
 
 if [[ "${version}" == "latest" ]] || [[ -n "${fetch}" ]]; then
     install_action_dir="${HOME}/.cache-cargo-install-action"
-    jq_use_b=''
     case "${host_os}" in
         linux*)
-            if ! type -P jq &>/dev/null || ! type -P curl &>/dev/null; then
+            if ! type -P jq >/dev/null || ! type -P curl >/dev/null; then
                 case "${base_distro}" in
                     debian | fedora | arch | alpine)
-                        echo "::group::Install packages required for installation (jq and/or curl)"
+                        printf '::group::Install packages required for installation (jq and/or curl)\n'
                         sys_packages=()
-                        if ! type -P curl &>/dev/null; then
+                        if ! type -P curl >/dev/null; then
                             sys_packages+=(ca-certificates curl)
                         fi
                         if [[ "${dnf:-}" == "yum" ]]; then
                             # On RHEL7-based distribution jq requires EPEL
-                            if ! type -P jq &>/dev/null; then
+                            if ! type -P jq >/dev/null; then
                                 sys_packages+=(epel-release)
                                 sys_install "${sys_packages[@]}"
                                 sys_install jq --enablerepo=epel
@@ -211,7 +211,7 @@ if [[ "${version}" == "latest" ]] || [[ -n "${fetch}" ]]; then
                                 sys_install "${sys_packages[@]}"
                             fi
                         else
-                            if ! type -P jq &>/dev/null; then
+                            if ! type -P jq >/dev/null; then
                                 # https://github.com/taiki-e/install-action/issues/521
                                 if [[ "${base_distro}" == "arch" ]]; then
                                     sys_packages+=(glibc)
@@ -220,64 +220,53 @@ if [[ "${version}" == "latest" ]] || [[ -n "${fetch}" ]]; then
                             fi
                             sys_install "${sys_packages[@]}"
                         fi
-                        echo "::endgroup::"
+                        printf '::endgroup::\n'
                         ;;
                     *) warn "cache-cargo-install-action requires jq and curl on non-Debian/Fedora/Arch/Alpine-based Linux" ;;
                 esac
             fi
             ;;
         macos)
-            if ! type -P jq &>/dev/null || ! type -P curl &>/dev/null; then
+            if ! type -P jq >/dev/null || ! type -P curl >/dev/null; then
                 warn "cache-cargo-install-action requires jq and curl on macOS"
             fi
             ;;
         windows)
-            if ! type -P curl &>/dev/null; then
+            if ! type -P curl >/dev/null; then
                 warn "cache-cargo-install-action requires curl on Windows"
             fi
-            # https://github.com/jqlang/jq/issues/1854
-            jq_use_b=1
-            jq="${install_action_dir}/jq/bin/jq.exe"
-            if [[ ! -f "${jq}" ]]; then
-                jq_version=$(jq --version || echo "")
-                case "${jq_version}" in
-                    jq-1.[7-9]* | jq-1.[1-9][0-9]*) jq='' ;;
-                    *)
-                        _tmp=$(jq <<<"{}" -r .a || echo "")
-                        if [[ "${_tmp}" == "null" ]]; then
-                            jq=''
-                            jq_use_b=''
-                        else
-                            info "old jq (${jq_version}) has bug on Windows; downloading jq 1.7 (will not be added to PATH)"
-                            mkdir -p "${install_action_dir}/jq/bin"
-                            url='https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-windows-amd64.exe'
-                            checksum='7451fbbf37feffb9bf262bd97c54f0da558c63f0748e64152dd87b0a07b6d6ab'
-                            (
-                                cd "${install_action_dir}/jq/bin"
-                                download_and_checksum "${url}" "${checksum}"
-                                mv tmp jq.exe
-                            )
-                            echo
-                        fi
-                        ;;
-                esac
+            if type -P jq >/dev/null; then
+                # https://github.com/jqlang/jq/issues/1854
+                _tmp=$(jq -r .a <<<'{}')
+                if [[ "${_tmp}" != "null" ]]; then
+                    _tmp=$(jq -b -r .a 2>/dev/null <<<'{}' || true)
+                    if [[ "${_tmp}" == "null" ]]; then
+                        jq() { command jq -b "$@"; }
+                    else
+                        jq() { command jq "$@" | tr -d '\r'; }
+                    fi
+                else
+                    printf '::group::Install packages required for installation (jq)\n'
+                    mkdir -p -- "${install_action_dir}/jq/bin"
+                    url='https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-windows-amd64.exe'
+                    checksum='7451fbbf37feffb9bf262bd97c54f0da558c63f0748e64152dd87b0a07b6d6ab'
+                    (
+                        cd -- "${install_action_dir}/jq/bin"
+                        download_and_checksum "${url}" "${checksum}"
+                        mv -- tmp jq.exe
+                    )
+                    printf '::endgroup::\n'
+                    jq() { "${install_action_dir}/jq/bin/jq.exe" -b "$@"; }
+                fi
             fi
             ;;
         *) bail "unsupported host OS '${host_os}'" ;;
     esac
-    call_jq() {
-        # https://github.com/jqlang/jq/issues/1854
-        if [[ -n "${jq_use_b}" ]]; then
-            "${jq:-jq}" -b "$@"
-        else
-            "${jq:-jq}" "$@"
-        fi
-    }
 
     crate_info=$(retry curl --proto '=https' --tlsv1.2 -fsSL --retry 10 "https://crates.io/api/v1/crates/${tool}")
     case "${version}" in
         latest)
-            version=$(call_jq <<<"${crate_info}" -r '.crate.max_stable_version')
+            version=$(jq -r '.crate.max_stable_version' <<<"${crate_info}")
             if [[ "${version}" == "null" ]]; then
                 bail "no stable version found for ${tool}; if you want to install a pre-release version, please specify the full version"
             fi
@@ -287,7 +276,8 @@ if [[ "${version}" == "latest" ]] || [[ -n "${fetch}" ]]; then
                 bail "cache-cargo-install-action does not support non-semver version: '${version}'"
             fi
             # shellcheck disable=SC2207
-            versions=($(call_jq <<<"${crate_info}" -r ".versions[] | select(.num | startswith(\"${version}.\")) | select(.yanked == false) | .num"))
+            versions=($(jq -r ".versions[] | select(.num | startswith(\"${version}.\")) | select(.yanked == false) | .num" <<<"${crate_info}"))
+            full_version=''
             for v in ${versions[@]+"${versions[@]}"}; do
                 if [[ ! "${v}" =~ ^[0-9]+\.[0-9]+\.[0-9]+(\+[0-9A-Za-z\.-]+)?$ ]]; then
                     continue
@@ -295,7 +285,7 @@ if [[ "${version}" == "latest" ]] || [[ -n "${fetch}" ]]; then
                 full_version="${v}"
                 break
             done
-            if [[ -z "${full_version:-}" ]]; then
+            if [[ -z "${full_version}" ]]; then
                 bail "no stable version  found for ${tool} that match with '${version}.*'; if you want to install a pre-release version, please specify the full version"
             fi
             version="${full_version}"
@@ -304,7 +294,7 @@ if [[ "${version}" == "latest" ]] || [[ -n "${fetch}" ]]; then
 fi
 
 bin_dir="${RUNNER_TOOL_CACHE}/${tool}/bin"
-echo "${bin_dir}" >>"${GITHUB_PATH}"
+printf '%s\n' "${bin_dir}" >>"${GITHUB_PATH}"
 
 if [[ -n "${git}" ]]; then
     key="${tool}-git-${tag:+"tag-${tag}"}${rev:+"rev-${rev}"}-${host_arch}-${host_os}${locked_key}"

.github/workflows/ci.yml
@@ -15,7 +15,7 @@ on:
 
 defaults:
   run:
-    shell: bash
+    shell: bash --noprofile --norc -CeEuxo pipefail {0}
 
 concurrency:
   group: ${{ github.workflow }}-${{ github.event.pull_request.number || github.sha }}
@@ -111,25 +111,23 @@ jobs:
     steps:
       - name: Install requirements (ubuntu/debian)
         run: |
-          set -eEuxo pipefail
           apt-get -o Acquire::Retries=10 -qq update
           apt-get -o Acquire::Retries=10 -o Dpkg::Use-Pty=0 install -y --no-install-recommends ca-certificates curl gcc libc6-dev
           curl --proto '=https' --tlsv1.2 -fsSL --retry 10 https://sh.rustup.rs | sh -s -- -y --profile minimal --default-toolchain stable --no-modify-path
-          echo "$HOME/.cargo/bin" >>"${GITHUB_PATH}"
+          printf '%s\n' "${HOME}/.cargo/bin" >>"${GITHUB_PATH}"
           apt-get -qq -o Dpkg::Use-Pty=0 remove -y curl
         if: startsWith(matrix.container, 'ubuntu') || startsWith(matrix.container, 'debian')
       - name: Install requirements (fedora/almalinux)
         run: |
-          set -eEuxo pipefail
-          if type -P dnf &>/dev/null; then
+          if type -P dnf >/dev/null; then
             dnf install -y gcc
-          elif type -P microdnf &>/dev/null; then
+          elif type -P microdnf >/dev/null; then
             microdnf install -y gcc
           else
             yum install -y gcc
           fi
           curl --proto '=https' --tlsv1.2 -fsSL --retry 10 https://sh.rustup.rs | sh -s -- -y --profile minimal --default-toolchain stable --no-modify-path
-          echo "$HOME/.cargo/bin" >>"${GITHUB_PATH}"
+          printf '%s\n' "${HOME}/.cargo/bin" >>"${GITHUB_PATH}"
         if: startsWith(matrix.container, 'fedora') || startsWith(matrix.container, 'almalinux')
       - name: Install requirements (archlinux)
         run: pacman -Sy --noconfirm rust

.github/workflows/release.yml
@@ -10,7 +10,7 @@ on:
 
 defaults:
   run:
-    shell: bash
+    shell: bash --noprofile --norc -CeEuxo pipefail {0}
 
 jobs:
   create-release:

.shellcheckrc
@@ -2,13 +2,34 @@
 # https://github.com/koalaman/shellcheck/blob/HEAD/shellcheck.1.md#rc-files
 
 # See also:
-# https://www.shellcheck.net/wiki/Optional
+# https://github.com/koalaman/shellcheck/wiki/Optional
 # https://google.github.io/styleguide/shellguide.html
 
-# https://www.shellcheck.net/wiki/SC2292
+# https://github.com/koalaman/shellcheck/wiki/SC2249
+# enable=add-default-case
+
+# https://github.com/koalaman/shellcheck/wiki/SC2244
+enable=avoid-nullary-conditions
+
+# https://github.com/koalaman/shellcheck/wiki/SC2312
+# enable=check-extra-masked-returns
+
+# https://github.com/koalaman/shellcheck/wiki/SC2310
+# https://github.com/koalaman/shellcheck/wiki/SC2311
+# enable=check-set-e-suppressed
+
+# enable=check-unassigned-uppercase
+
+# https://github.com/koalaman/shellcheck/wiki/SC2230
+enable=deprecate-which
+
+# https://github.com/koalaman/shellcheck/wiki/SC2248
+enable=quote-safe-variables
+
+# https://github.com/koalaman/shellcheck/wiki/SC2292
 # https://google.github.io/styleguide/shellguide.html#s6.3-tests
 enable=require-double-brackets
 
-# https://www.shellcheck.net/wiki/SC2250
+# https://github.com/koalaman/shellcheck/wiki/SC2250
 # https://google.github.io/styleguide/shellguide.html#s5.6-variable-expansion
 enable=require-variable-braces

action.yml
@@ -47,6 +47,7 @@ runs:
     - name: Install ${{ inputs.tool }}
       run: |
         export CARGO_NET_RETRY=10
+        # shellcheck disable=SC2206
         args=(-f "${{ steps.pre.outputs.tool }}" --root "${RUNNER_TOOL_CACHE}/${{ steps.pre.outputs.tool }}" ${{ steps.pre.outputs.locked }})
         if [[ -n "${{ steps.pre.outputs.git }}" ]]; then
           if [[ -n "${{ steps.pre.outputs.tag }}" ]]; then

tools/publish.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: Apache-2.0 OR MIT
-set -eEuo pipefail
+set -CeEuo pipefail
 IFS=$'\n\t'
-cd "$(dirname "$0")"/..
-
-# shellcheck disable=SC2154
-trap 's=$?; echo >&2 "$0: error on line "${LINENO}": ${BASH_COMMAND}"; exit ${s}' ERR
+trap -- 's=$?; printf >&2 "%s\n" "${0##*/}:${LINENO}: \`${BASH_COMMAND}\` exit with ${s}"; exit ${s}' ERR
+cd -- "$(dirname -- "$0")"/..
 
 # Publish a new release.
 #
@@ -26,7 +24,7 @@ retry() {
     "$@"
 }
 bail() {
-    echo >&2 "error: $*"
+    printf >&2 'error: %s\n' "$*"
     exit 1
 }
 
@@ -41,6 +39,11 @@ fi
 if [[ $# -gt 1 ]]; then
     bail "invalid argument '$2'"
 fi
+if { sed --help 2>&1 || true; } | grep -Eq -e '-i extension'; then
+    in_place=(-i '')
+else
+    in_place=(-i)
+fi
 
 # Make sure there is no uncommitted change.
 git diff --exit-code
@@ -51,12 +54,16 @@ if gh release view "${tag}" &>/dev/null; then
     bail "tag '${tag}' has already been created and pushed"
 fi
 
-if ! git branch | grep -q '\* main$'; then
+# Make sure that the release was created from an allowed branch.
+if ! git branch | grep -Eq '\* main$'; then
     bail "current branch is not 'main'"
+fi
+if ! git remote -v | grep -F origin | grep -Eq 'github\.com[:/]taiki-e/'; then
+    bail "cannot publish a new release from fork repository"
 fi
 
 release_date=$(date -u '+%Y-%m-%d')
-tags=$(git --no-pager tag | (grep -E "^${tag_prefix}[0-9]+" || true))
+tags=$(git --no-pager tag | { grep -E "^${tag_prefix}[0-9]+" || true; })
 if [[ -n "${tags}" ]]; then
     # Make sure the same release does not exist in changelog.
     if grep -Eq "^## \\[${version//./\\.}\\]" "${changelog}"; then
@@ -66,11 +73,12 @@ if [[ -n "${tags}" ]]; then
         bail "link to ${version} already exist in ${changelog}"
     fi
     # Update changelog.
-    remote_url=$(grep -E '^\[Unreleased\]: https://' "${changelog}" | sed 's/^\[Unreleased\]: //; s/\.\.\.HEAD$//')
+    remote_url=$(grep -E '^\[Unreleased\]: https://' "${changelog}" | sed -E 's/^\[Unreleased\]: //; s/\.\.\.HEAD$//')
     prev_tag="${remote_url#*/compare/}"
     remote_url="${remote_url%/compare/*}"
-    sed -i "s/^## \\[Unreleased\\]/## [Unreleased]\\n\\n## [${version}] - ${release_date}/" "${changelog}"
-    sed -i "s#^\[Unreleased\]: https://.*#[Unreleased]: ${remote_url}/compare/${tag}...HEAD\\n[${version}]: ${remote_url}/compare/${prev_tag}...${tag}#" "${changelog}"
+    sed -E "${in_place[@]}" \
+        -e "s/^## \\[Unreleased\\]/## [Unreleased]\\n\\n## [${version}] - ${release_date}/" \
+        -e "s#^\[Unreleased\]: https://.*#[Unreleased]: ${remote_url}/compare/${tag}...HEAD\\n[${version}]: ${remote_url}/compare/${prev_tag}...${tag}#" "${changelog}"
     if ! grep -Eq "^## \\[${version//./\\.}\\] - ${release_date}$" "${changelog}"; then
         bail "failed to update ${changelog}"
     fi
@@ -93,9 +101,9 @@ changes=$(parse-changelog "${changelog}" "${version}")
 if [[ -z "${changes}" ]]; then
     bail "changelog for ${version} has no body"
 fi
-echo "============== CHANGELOG =============="
-echo "${changes}"
-echo "======================================="
+printf '============== CHANGELOG ==============\n'
+printf '%s\n' "${changes}"
+printf '=======================================\n'
 
 if [[ -n "${tags}" ]]; then
     # Create a release commit.
@@ -120,7 +128,6 @@ if git --no-pager tag | grep -Eq "^${major_version_tag}$"; then
     retry git push --delete origin refs/tags/"${major_version_tag}"
 fi
 git tag "${major_version_tag}"
+retry git push origin --tags
 git checkout main
 git branch -d "${major_version_tag}"
-
-retry git push origin --tags

commit 4fb6dd7e39ac8fb5ad236df02e62c98226ab389d

Fix jq handling on Windows
Fix regression introduced in e7484e2d4deec5779a5c8f55f88cf522978898bd.

pre.sh
@@ -235,7 +235,9 @@ if [[ "${version}" == "latest" ]] || [[ -n "${fetch}" ]]; then
       if ! type -P curl >/dev/null; then
         warn "cache-cargo-install-action requires curl on Windows"
       fi
-      if type -P jq >/dev/null; then
+      if [[ -f "${install_action_dir}/jq/bin/jq.exe" ]]; then
+        jq() { "${install_action_dir}/jq/bin/jq.exe" -b "$@"; }
+      elif type -P jq >/dev/null; then
         # https://github.com/jqlang/jq/issues/1854
         _tmp=$(jq -r .a <<<'{}')
         if [[ "${_tmp}" != "null" ]]; then
@@ -245,19 +247,19 @@ if [[ "${version}" == "latest" ]] || [[ -n "${fetch}" ]]; then
           else
             jq() { command jq "$@" | tr -d '\r'; }
           fi
-        else
-          printf '::group::Install packages required for installation (jq)\n'
-          mkdir -p -- "${install_action_dir}/jq/bin"
-          url='https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-windows-amd64.exe'
-          checksum='7451fbbf37feffb9bf262bd97c54f0da558c63f0748e64152dd87b0a07b6d6ab'
-          (
-            cd -- "${install_action_dir}/jq/bin"
-            download_and_checksum "${url}" "${checksum}"
-            mv -- tmp jq.exe
-          )
-          printf '::endgroup::\n'
-          jq() { "${install_action_dir}/jq/bin/jq.exe" -b "$@"; }
         fi
+      else
+        printf '::group::Install packages required for installation (jq)\n'
+        mkdir -p -- "${install_action_dir}/jq/bin"
+        url='https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-windows-amd64.exe'
+        checksum='7451fbbf37feffb9bf262bd97c54f0da558c63f0748e64152dd87b0a07b6d6ab'
+        (
+          cd -- "${install_action_dir}/jq/bin"
+          download_and_checksum "${url}" "${checksum}"
+          mv -- tmp jq.exe
+        )
+        printf '::endgroup::\n'
+        jq() { "${install_action_dir}/jq/bin/jq.exe" -b "$@"; }
       fi
       ;;
     *) bail "unsupported host OS '${host_os}'" ;;